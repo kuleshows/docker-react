a95560f3bb2464632f9c6a1b51b41b01d2ac87db
Sergei Kuleshov <skuleshov@deloitte.com>
2019-04-18 11:55:12 +0300

    random change to test travis

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React. Yo-ho-ho ho11
+            Learn React. Yo-ho-ho ho11 1
           </a>
         </header>
       </div>
